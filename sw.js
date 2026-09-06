fe8ffa24c2eda976c050b566c518d1b010593af1
Usama Gulzar <ugulzar.bscs25seecs@seecs.edu.pk>
2026-09-06 15:27:55 +0500

    chore: bump cache versions to bust PWA and browser cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mantiq-cache-v2.2.65'; // bumped to drop marketing page from cache
+const CACHE_NAME = 'mantiq-cache-v2.2.66'; // bumped to drop marketing page from cache
 
 const urlsToCache = [
   './app.html',
